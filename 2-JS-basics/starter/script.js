bbcb18b38f5c3489b6010d032340dcae6e510e4d
Semen Pleshivtsev <PleshivtsevSV@pgkweb.ru>
2018-12-11 11:21:58 +0300

    first commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,42 @@
+/**************************************************
+* Variables and data types
+*
+
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// variable nameing rules
+var _3years = 3;
+var johnMarrk = 'John and Mark';
+*/ 
+
+/**************************************************
+* Variables mitation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+alert('Is John married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(lastName);
